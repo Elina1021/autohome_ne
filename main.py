96b12b45aec43a8ea70609bc04d41ecb9f628c9c
solidjoker <solid_ye@163.com>
2019-08-13 17:26:59 +0800

    restructure for spider fix main

---
+++ main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Author: solidjoker
+# @Date:   2019-08-13 17:25:44
+# @Last Modified by:   solidjoker
+# @Last Modified time: 2019-08-13 17:28:24
+
+from tools import main
+
+if __name__ == '__main__':
+    df = main.main()
---
+++ tools/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Author: solidjoker
+# @Date:   2019-08-13 16:45:08
+# @Last Modified by:   solidjoker
+# @Last Modified time: 2019-08-13 16:45:11
